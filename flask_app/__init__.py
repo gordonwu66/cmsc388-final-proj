b46668f49c3788b693e449526e3f71a121fa0af4
Viraj <viraj797@yahoo.com>
2020-05-21 05:13:39 -0400

    Added heroku optional settings

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -28,7 +28,7 @@ csp = {
 }
 Talisman(app, content_security_policy=csp)
 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/second_database"
+# app.config["MONGO_URI"] = 'mongodb://heroku_1g94z7ls:6jah3cqn8h46fqlogsjfhhic9m@ds161109.mlab.com:61109/heroku_1g94z7ls?retryWrites=false'
 app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/project_4'
 app.config['SECRET_KEY'] = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
 
